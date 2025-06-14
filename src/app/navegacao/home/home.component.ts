719a6123ca893d16a69af7d0aa1cbc42abbd16a1
GabrielRP17 <gabriel.passos.r@gmail.com>
2025-06-14 15:17:15 -0300

    Atualizações

--- src/app/navegacao/home/home.component.ts
+++ src/app/navegacao/home/home.component.ts
@@ -1,36 +1,40 @@
-import { Component } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: true,
-    navSpeed: 700,
-    navText: ['<', '>'],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 1,
-      },
-      740: {
-        items: 1,
-      },
-      940: {
-        items: 1,
-      }
-    },
-    nav: true
+  images: string[] = [
+    'assets/Imagens/Carousel/Poster_DezConto.jpg',
+    'assets/Imagens/Carousel/Getulio_DoisConto.jpg',
+    'assets/Imagens/Carousel/Franca_DoisConto.jpg'
+  ];
+
+  currentIndex: number = 0;
+  intervalId: any;
+
+  ngOnInit(): void {
+    this.startAutoSlide();
   }
 
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  startAutoSlide(): void {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 3000); // Troca de slide a cada 3 segundos
+  }
+
+  stopAutoSlide(): void {
+    clearInterval(this.intervalId);
+  }
 }
